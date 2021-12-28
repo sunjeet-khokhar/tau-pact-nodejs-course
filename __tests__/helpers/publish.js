212f72dc0a5d662bb79342a10c097de05531644c
Sunjeet Khokhaer <sunjeet81@gmail.com>
2021-12-29 00:08:39 +1300

    working with pactflow.io

--- __tests__/helpers/publish.js
+++ __tests__/helpers/publish.js
@@ -3,7 +3,8 @@ let path = require("path")
 
 let opts = {
     pactFilesOrDirs: [path.resolve(process.cwd(), "__tests__/contract/pacts")],
-    pactBroker: "http://localhost:8080",
+    pactBroker: "https://trivialsearch.pactflow.io/",
+    pactBrokerToken: 'HTAxLcRZ8iHhT_12HevZYA',
     consumerVersion: "1.0.1",
     providerVersion: "1.0.1",
     tags: "dev"
